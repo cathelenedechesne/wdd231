f653f8f95e2528c3a0b541138f359e8ba7aaf7cd
cathelenedechesne <spjcat@gmail.com>
2024-06-12 20:51:05 +1000

    html adjustments + css starts + js for cards elements and footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// --- Footer Content ---
+
 // Get the current year
 const currentYear = new Date().getFullYear();
 
@@ -10,3 +12,21 @@ const lastModifiedDate = document.lastModified;
 // Update the content of the paragraph with id "lastModified" to display the last modified date
 document.getElementById("lastModified").textContent = "Last modified: " + lastModifiedDate;
 
+
+// --- "Web and Computer Programming Certificate" Section" ---
+function showContent(id) {
+    // Hide all content divs
+    var contents = document.querySelectorAll('.content');
+    contents.forEach(function (content) {
+        content.classList.remove('active');
+    });
+
+    if (id === 'content1') {
+        // Show the content for button 1, 2, and 3
+        document.getElementById('content2').classList.add('active');
+        document.getElementById('content3').classList.add('active');
+    } else {
+        // Show the selected content div
+        document.getElementById(id).classList.add('active');
+    }
+}
