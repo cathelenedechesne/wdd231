3692f13b82354cbe1f0b3c8dbbe391a1b5d41ea2
cathelenedechesne <spjcat@gmail.com>
2024-07-08 20:39:22 +1000

    join page and thankyou page created.

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -202,4 +202,47 @@ document.addEventListener('DOMContentLoaded', () => {
 
     fetchWeatherData(urlCurrent, displayCurrentWeather);
     fetchWeatherData(urlForecast, displayForecast);
+
+});
+
+//join.html
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Set the timestamp field
+    let timestampField = document.getElementById('timestamp');
+    let currentDateTime = new Date().toISOString();
+    timestampField.value = currentDateTime;
+
+    // Modal functionality
+    let modal = document.getElementById("membershipModal");
+    let btn = document.getElementById("learnMoreBtn");
+    let span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function (event) {
+        event.preventDefault();  // Prevent the default link behavior
+        modal.style.display = "block";
+    }
+
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
+
+//Display on thankyou.html
+document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    document.getElementById('first-name').textContent = urlParams.get('first');
+    document.getElementById('last-name').textContent = urlParams.get('last');
+    document.getElementById('email').textContent = urlParams.get('email');
+    document.getElementById('phone').textContent = urlParams.get('phone');
+    document.getElementById('organization').textContent = urlParams.get('organization-business-name');
+    document.getElementById('organizational').textContent = urlParams.get('organizational');
+    document.getElementById('membership').textContent = urlParams.get('membership');
+    document.getElementById('description').textContent = urlParams.get('description');
+    document.getElementById('timestamp').textContent = urlParams.get('timestamp');
 });
