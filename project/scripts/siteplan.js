f6ce3243ed7815cedbfbd0a2e79180ae029d26b8
cathelenedechesne <spjcat@gmail.com>
2024-07-14 19:31:00 +1000

    project folder and its content's creation + site plan done.

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,13 @@
+// --- Footer Content ---
+
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Update the content of the span with id "2024" to display the current year
+document.getElementById("2024").textContent = currentYear;
+
+// Get the last modified date of the document
+const lastModifiedDate = document.lastModified;
+
+// Update the content of the paragraph with id "lastModified" to display the last modified date
+document.getElementById("lastModified").textContent = "Last modified: " + lastModifiedDate;
